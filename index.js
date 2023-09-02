0f093dd562d889d79d1d1de71c1b4f10f4f6df52
Sadid-Hasan <sadid.hasan94@gmail.com>
2023-09-02 19:49:06 +0600

    responsive card width fixed

--- index.js
+++ index.js
@@ -1,57 +1,53 @@
-const handleCategory =async () =>{
-    const response = await fetch(
-        "https://openapi.programming-hero.com/api/videos/categories"
-    );
-    const data = await response.json();
-    const tabContainer = document.getElementById("tab-container");
-    const trimeData =data.data.slice(0,4);
-    trimeData.forEach((category) =>{
-       // console.log(category)
-        const div = document.createElement("div");
-        div.innerHTML =`
-        <a onclick="handleLoadnews('${category.category_id}')" class ="tab">${category.category}</a>
-        `;
-        tabContainer.appendChild(div);
-    });
+const handleCategory = async () => {
+  const response = await fetch(
+    "https://openapi.programming-hero.com/api/videos/categories"
+  );
+  const data = await response.json();
+  const tabContainer = document.getElementById("tab-container");
+  const trimeData = data.data.slice(0, 4);
+  trimeData.forEach((category) => {
+    const div = document.createElement("div");
+    div.innerHTML = `
+      <a onclick="handleLoadnews('${category.category_id}')" class="tab btn btn-xs  text-justify rounded-sm  md:btn-md lg:w-20">${category.category}</a>
+    `;
+    tabContainer.appendChild(div);
+  });
 };
- 
+
 const handleLoadnews = async (categoryId) => {
-    const response = await fetch (
-        `https://openapi.programming-hero.com/api/videos/category/${categoryId}`
-    );
-    const data =await response.json();
-    const cardContainer = document.getElementById("card-container");
-    data.data.forEach((news)=>{
-        console.log(news);
-        const div = document.createElement('div');
-        div.innerHTML = `
-        <div class="card bg-base-100 shadow-xl">
+  const response = await fetch(
+    `https://openapi.programming-hero.com/api/videos/category/${categoryId}`
+  );
+  const data = await response.json();
+  const cardContainer = document.getElementById("card-container");
+  data.data.forEach((news) => {
+    const div = document.createElement("div");
+    div.innerHTML = `
+      <div class="card bg-base-100 shadow-xl">
         <figure>
-          <img
-            src=${news.thumbnail}
-          />
+          <img  class ="max-w-sm h-64" src="${news.thumbnail}" />
         </figure>
         <div class="card-body">
-        <div class="flex">
-              <div>
-                <div class="avatar online">
-                  <div class="w-14 rounded-full">
-                    <img
-                      src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dXNlcnxlbnwwfHwwfHw%3D&w=1000&q=80"
-                    />
-                  </div>
+          <div class="flex">
+            <div>
+              <div class="avatar online">
+                <div class="w-14 rounded-full">
+                  <img src="${news.authors[0].profile_picture}" />
                 </div>
               </div>
-              <div>
-          <h2 class="card-title">
-            Biden Pledges Nearly $3 Billion To Ukraine
-          </h2>
-          <h6>Jimmy Dane</h6>
-          <small>2022-08-24 17:27:34</small>
+            </div>
+            <div>
+              <h2 class="card-title">
+                ${news.title}
+              </h2>
+              <h6>${news.authors[0].profile_name}</h6>
+              <h7>${news.others.views} views</h7>
+            </div>
+          </div>
         </div>
       </div>
-      `;
-      cardContainer.appendChild(div);
-    });
+    `;
+    cardContainer.appendChild(div);
+  });
 };
 handleCategory();
